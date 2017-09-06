bb93164ec62a68e96198f6de72183de1c8f670c8
golnazads <28757512+golnazads@users.noreply.github.com>
2017-09-06 11:20:08 -0400

    using sqlalchemy to query db

--- linksData.py
+++ linksData.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import current_app
-import psycopg2
-from config import config
+from sqlalchemy import create_engine
+import sqlalchemy.exc
 
 def getURL(bibcode, linkType):
     query = "SELECT linkURL FROM linkInfo as I INNER JOIN linkTypes as T ON I.linkTypeId = T.linkTypeID WHERE I.bibcode = '{}' AND T.linkTypeName = '{}'".format(bibcode, linkType)
@@ -17,23 +17,17 @@ def getLinkTag(linkTypeName):
 
 def executeQuery(query,errorMsg):
     result = ""
-    conn = None
     try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
+        engine = create_engine(current_app.config['SQLALCHEMY_URL'])
+        conn = engine.connect()
+
         # submit the query to get the link URL
-        cur.execute(query)
-        rows = cur.fetchall()
+        rows = conn.execute(query).fetchall()
+        print '-------------', rows
         if len(rows) > 0:
             result = rows[0][0]
-        # close communication with the PostgreSQL database server
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
+
+    except (Exception, sqlalchemy.exc.DatabaseError) as error:
         current_app.logger.error(errorMsg, error)
-    finally:
-        if conn is not None:
-            conn.close()
+
     return result
