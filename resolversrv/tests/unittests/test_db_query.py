9ba8c3f8a580e9dae0f32714bd996c1a79f8dde6
golnazads <28757512+golnazads@users.noreply.github.com>
2018-05-06 17:50:41 -0400

    testing the tests

--- resolversrv/tests/unittests/test_db_query.py
+++ resolversrv/tests/unittests/test_db_query.py
@@ -99,9 +99,10 @@ class test_database(TestCase):
                                                      'url': record[3], 'title': record[4],
                                                      'item_count': record[5]}]}
             record_list_msg.datalinks_records.add(**datalinks_record)
+        self.assertNotEqual(self.current_app.session_scope(), None)
         status, text = add_records(record_list_msg)
         self.assertEqual(status, True)
-        # self.assertEqual(text, 'updated db with new data successfully')
+        self.assertEqual(text, 'updated db with new data successfully')
 
 
     def test_add_records_no_data(self):
